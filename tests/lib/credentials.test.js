855bf27051c3a1a7f5b7471b45cc56206d1da23b
Saïd Mimouni <sayid.mimouni@gmail.com>
2020-10-03 22:41:45 +0200

    Store credentials in home directory

--- tests/lib/credentials.test.js
+++ tests/lib/credentials.test.js
@@ -1,14 +1,15 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const credentials = require('../../lib/credentials');
 
-const rootDir = path.resolve('./');
+const homeDir = os.homedir();
 
 describe('Credentials', () => {
   describe('#get', () => {
     describe('when there is no credentials file', () => {
       test('rejects', () => {
-        fs.unlinkSync(rootDir + '/.credentials.test.json');
+        fs.unlinkSync(homeDir + '/.konsole.test.json');
         return expect(credentials.get()).rejects.toThrow('No credentials found')
       });
     });
@@ -28,7 +29,7 @@ describe('Credentials', () => {
         url: 'postgres://postgres:password@localhost/foo',
         token: 'qwerty',
       });
-      fs.readFile(rootDir + '/.credentials.test.json', (err, data) => {
+      fs.readFile(homeDir + '/.konsole.test.json', (err, data) => {
         if (err) throw err;
         const credentials = JSON.parse(data.toString());
         expect(credentials).toEqual({
