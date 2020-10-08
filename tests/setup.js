618aa29a1be4a8103d42b7bd8016a00fc8bfdaa2
Saïd Mimouni <sayid.mimouni@gmail.com>
2020-10-08 23:23:16 +0200

    Remove test credentials after test suite

--- tests/setup.js
+++ tests/setup.js
@@ -1,7 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
 const fixtures = require('./fixtures');
 const credentials = require('../lib/credentials');
+
+const homeDir = os.homedir();
 
 global.beforeEach(async () => {
   await credentials.set({
@@ -10,4 +13,8 @@ global.beforeEach(async () => {
   });
   await fixtures.clean();
   return fixtures.insert();
+});
+
+global.afterAll(() => {
+  fs.unlinkSync(homeDir + '/.konsole.test.json');
 });
